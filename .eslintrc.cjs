5e60e96348d7e419c1f6130ba62e10faa15885eb
ShiriNmi1520 <s20026352@gmail.com>
2022-10-17 00:48:30 +0800

    refactor: JavaScript Re-implement restify sample server from typescript to javasript Re-generate eslint config file Re-generate jest config file

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb-base'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-};
--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import * as restify from restify
-import {Server} from "restify";
-
-export class ostbServer {
-  private restify: Server
-
-  const server = restify.createServer()
-}
